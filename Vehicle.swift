1c5dfc14db31dc440d9ca1d82d3898ca9f794977
jaeyoonlee2 <>
2021-05-26 03:52:51 +0000

    Create Vehicle.swift and main.swift

---
+++ Vehicle.swift
@@ -0,0 +1,86 @@
+/*
+This class use contructor.
+author  Jay Lee
+version 1.0
+since   2021-05-25
+*/
+
+class Vehicle {
+  // Fields
+  private var licensePlateNumber, colour: String
+  private let numberOfDoors, maxSpeed: Int
+  private var speed: Int
+
+  // Constructor
+  init(licensePlateNumber: String, colour: String, numberOfDoors: Int, maxSpeed: Int) {
+    self.licensePlateNumber = licensePlateNumber
+    self.colour = colour
+    self.numberOfDoors = numberOfDoors
+    self.maxSpeed = maxSpeed
+    self.speed = 0
+  }
+
+  // This method prints to console all the properties.
+  func PrintVehicleInfo() {
+    print("License plate number: \(self.GetLicensePlateNumber())")
+    print("Colour:  \(self.GetColour())")
+    print("Number of doors : \(String(GetNumberOfDoors()))")
+    print("Max speed: \(self.GetMaxSpeed())km/h")
+    print("Current speed: \(self.GetSpeed())km/h")
+  }
+
+// This method accelerates the speed.
+  func Accelerate(increment: Int) {
+    if (speed + increment <= maxSpeed) {
+      self.speed += increment
+    } else {
+      print("The vehicle is at max speed!")
+      self.speed = maxSpeed
+    }
+  }
+
+  // This method decelerates the speed.
+  func Brake(decrement: Int) {
+    if (speed - decrement >= 0) {
+      self.speed -= decrement
+    } else {
+      print("The vehicle has stopped!")
+      self.speed = 0
+    }
+  }
+
+  // This method returns the license plate number.
+  func GetLicensePlateNumber() -> String{
+    return self.licensePlateNumber
+  }
+
+  // This method returns the colour of the vehicle.
+  func GetColour() -> String {
+    return self.colour
+  }
+
+  // This method returns the number of doors.
+  func GetNumberOfDoors() -> Int {
+    return self.numberOfDoors
+  }
+
+  // This method returns the current speed of vehicle.
+  func GetSpeed() -> Int {
+    return self.speed
+  }
+
+  // This method returns the max speed.
+  func GetMaxSpeed() -> Int {
+    return self.maxSpeed
+  }
+
+  // This method gets new license plate number and sets it.
+  func SetLicensePlateNumber(newLicensePlateNumber: String) {
+    self.licensePlateNumber = newLicensePlateNumber
+  }
+
+  // This method gets new colour and sets it.
+  func SetColour(newColour: String) {
+    self.colour = newColour
+  }
+}
---
+++ main.swift
@@ -0,0 +1,63 @@
+/*
+This program uses the Vehicle class with constructor.
+author  Jay Lee
+version 1.0
+since   2021-05-25
+*/
+
+enum InvalidInputError : Error {
+  case invalidInput
+}
+
+
+do {
+  print("Enter the license plate number: ", terminator:"")
+  let licensePlateNumber = String(readLine()!)
+  print("Enter the colour of the car: ", terminator:"")
+  let colour = String(readLine()!)
+  print("Enter the number of doors: ", terminator:"")
+  guard let numberOfDoors = Int(readLine()!) else {
+    throw InvalidInputError.invalidInput
+  }
+  print("Enter the max speed(km/h): ", terminator:"")
+  guard let maxSpeed = Int(readLine()!) else {
+    throw InvalidInputError.invalidInput
+  }
+
+  let myVehicle = Vehicle(licensePlateNumber: licensePlateNumber, colour: colour, numberOfDoors: numberOfDoors, maxSpeed: maxSpeed)
+
+  print("")
+
+  while(true) {
+    print("Enter the command: ", terminator:"")
+    let inputString = String(readLine()!).uppercased()
+    if (inputString == "END") {
+      break
+    } else if (inputString == "ACCELERATE") {
+      print("Enter the value to be accelerated (km/h): ", terminator:"")
+      guard let increment: Int = Int(readLine()!) else {
+        throw InvalidInputError.invalidInput
+      }
+      myVehicle.Accelerate(increment: increment)
+    } else if (inputString == "BRAKE") {
+      print("Enter the value to be decelerated (km/h):  ", terminator:"")
+      guard let decrement = Int(readLine()!) else {
+        throw InvalidInputError.invalidInput
+      }
+      myVehicle.Brake(decrement: decrement)
+    } else if (inputString == "LICENSE") {
+      print("Enter the new license plate number: ", terminator:"")
+      myVehicle.SetLicensePlateNumber(newLicensePlateNumber: String(readLine()!))
+    } else if (inputString == "COLOUR") {
+      print("Enter the new colour of the car: ", terminator:"")
+      myVehicle.SetColour(newColour: String(readLine()!))
+    } else {
+      continue
+    }
+  }
+  print("\nMy Vehicle: ")
+  myVehicle.PrintVehicleInfo()
+} catch {
+  print("\nInvalid input.")
+}
+print("\nDone.")
